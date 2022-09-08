53513dc0f0ed96da261ba9b56ad5070e0abcbc66
Samuel <101745495+bytutscode@users.noreply.github.com>
2022-09-08 16:38:21 -0300

    Update quiz.js

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -7,7 +7,7 @@ let questions = [
             'tag scripting',
             'tag javascript'
         ],
-        answer: 2
+        answer: 'tag script'
     },
     {
         question: 'Onde é o local certo de colocar o javascript?',
@@ -16,7 +16,7 @@ let questions = [
             'Na tag head',
             'Tanto na tag body quanto a head estão corretas'
         ],
-        answer: 3
+        answer: 'Tanto na tag body quanto a head estão corretas'
     },
     {
         question: 'Qual a sintaxe correta para adicionar um arquivo javascript externo na tag script?',
@@ -25,7 +25,7 @@ let questions = [
             'src="xxx.js"',
             'href="xxx.js"'
         ],
-        answer: 2
+        answer: 'src="xxx.js"'
     },
     {
         question: 'Um arquivo javascript externo precisa ter a tag script?',
@@ -33,7 +33,7 @@ let questions = [
             'Verdade',
             'Falso'
         ],
-        answer: 2
+        answer: 'Falso'
     },
     {
         question: 'Como escrever um "Olá Mundo" em um alertbox?',
@@ -43,6 +43,22 @@ let questions = [
             'msg("Olá Mundo")',
             'alert("Olá Mundo")'
         ],
-        answer: 4
+        answer: 'alert("Olá Mundo")'
     }
 ];
+
+function shuffle (array) {
+    let vetorx = [];
+    let length = array.length;
+        for(let i,j,x = 0; x < length; x++){
+            i = parseInt(Math.random()*array.length);
+            j = array.splice(i,1);
+            vetorx.push(j[0]);
+        }
+        for(i = 0; i < vetorx.length; i++){
+            array.push(vetorx[i]);
+        }
+}
+shuffle(questions);
+
+
